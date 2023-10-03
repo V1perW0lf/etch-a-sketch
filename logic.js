e69cf07185a0658a3cf167e578d4e067ab167b28
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-10-03 09:15:04 -0500

    add button that lets you change grid size

--- logic.js
+++ logic.js
@@ -1,16 +1,42 @@
-const gridContainer = document.querySelector("div.grid-container");
-let gridArray = []
-
-for (let i = 0; i < 16; i++) {
-    gridArray[i] = [];
-    for (let j = 0; j < 16; j++) {
-        console.log(i);
-        console.log(j);
-        gridArray[i][j] = document.createElement('div');
-        gridArray[i][j].className = 'grid-square';
-        gridArray[i][j].addEventListener("mouseover", () => {
-            gridArray[i][j].style.backgroundColor = 'blue';
-        })
-        gridContainer.appendChild(gridArray[i][j]);
+function createGrid(size = 16) {
+    const gridContainer = document.querySelector("div.grid-container");
+    const gridContainerWidth = gridContainer.offsetWidth;
+    const BORDER_SIZE = '1';
+    const squareSize = ((gridContainerWidth/size) - (+BORDER_SIZE * 2)).toString() + 'px';
+    let gridArray = [];
+    
+    for (let i = 0; i < size; i++) {
+        gridArray[i] = [];
+        for (let j = 0; j < size; j++) {
+            gridArray[i][j] = document.createElement('div');
+            gridArray[i][j].className = 'grid-square';
+            gridArray[i][j].style.border = `${BORDER_SIZE}px solid black`;
+            gridArray[i][j].style.width = squareSize;
+            gridArray[i][j].style.height = squareSize;
+            gridArray[i][j].addEventListener("mouseover", () => {
+                gridArray[i][j].style.backgroundColor = 'blue';
+            })
+            gridContainer.appendChild(gridArray[i][j]);
+        }
+        const lineBreak = document.createElement('div');
+        lineBreak.className = "break";
+        gridContainer.appendChild(lineBreak);
     }
 }
+
+const gridSizeButton = document.querySelector('button.square-prompt');
+gridSizeButton.addEventListener('click', () => {
+    let newGridSize = +prompt("Please enter a new grid size. Grids cannot be larger than 100.");
+    if(newGridSize > 100) {
+        newGridSize = 100;
+    }
+    
+    const oldGrids = document.querySelectorAll('div.grid-square');
+    oldGrids.forEach((square) => {
+        square.remove();
+    })
+
+    createGrid(newGridSize);
+})
+
+createGrid();
